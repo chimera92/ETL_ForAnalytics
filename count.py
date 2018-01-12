550435d8723cb22b9a3e426a5a256c1908430e3c
chimera <hemanth.bm12@gmail.com>
2018-01-12 04:04:15 -0800

    ETL done.. mapping pending

---
+++ count.py
@@ -0,0 +1,32 @@
+import pymongo
+client = pymongo.MongoClient('18.218.79.6', 27017)
+client = pymongo.MongoClient('localhost', 27017)
+database = client["EntityDB"]
+count=0
+collections=database.collection_names()
+# collections.remove("system.indexes")
+OFL_count=0
+EXP_count=0
+for coll in collections:
+    collection = database[coll]
+
+    OFL_CountryCount=collection.find({'_id': {'$regex': '^OFL_'}}).count()
+    OFL_count+=OFL_CountryCount
+
+    EXP_CountryCount = collection.find({'_id': {'$regex': '^EXP_'}}).count()
+    EXP_count += EXP_CountryCount
+
+    print "Country: {} -> Offshore Leaks count: {}, US Consolidated Screening List count:{}".format(collection.name,OFL_CountryCount,EXP_CountryCount)
+print "\n\nTotal Offshore Leaks count:{}".format(OFL_count)
+print "Total US Consolidated Screening List count:{}".format(EXP_count)
+print "Total count:{}".format(OFL_count+EXP_count)
+# Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+
+# query = {}
+#
+# cursor = collection.find(query)
+# try:
+#     for doc in cursor:
+#         print doc["_id"]
+# finally:
+#     cursor.close()
--- updater.py
+++ updater.py
@@ -3,7 +3,6 @@ import datetime
 import time
 from StringIO import StringIO
 import eventlet
-
 import pandas as pd
 import pymongo
 import json
@@ -11,25 +10,43 @@ from geopy.geocoders import GoogleV3
 from concurrent.futures import ThreadPoolExecutor
 import requests
 import zipfile
-
+import logging
 from io import BytesIO
+import sys
 
-mongoClient = pymongo.MongoClient('localhost', 27017)
-geolocator = GoogleV3(api_key='AIzaSyCV2dxrI4RItaKqOoenXr5osbEqazE6lYs')
-ex = ThreadPoolExecutor(max_workers=200)
 eventlet.monkey_patch()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+sout_log_handler = logging.StreamHandler(sys.stdout)
+file_log_handler = logging.FileHandler("updater.log")
+sout_log_handler.setLevel(logging.DEBUG)
+file_log_handler.setLevel(logging.DEBUG)
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+sout_log_handler.setFormatter(log_formatter)
+file_log_handler.setFormatter(log_formatter)
+logger.addHandler(sout_log_handler)
+logger.addHandler(file_log_handler)
+
+geolocator = GoogleV3(api_key='xxx')
+ex = ThreadPoolExecutor(max_workers=200)
+
+DB_IP = "xxx"
+PORT = 27017
+logger.info('Connecting to database at "{}" port "{}"'.format(DB_IP, PORT))
+mongoClient = pymongo.MongoClient(DB_IP, PORT)
+
 
 def get_lat_long(address):
     LatLongCollection = mongoClient['LatLongDB']['LatLongCollection']
 
-    lat_long=LatLongCollection.find_one({'_id': address})
-    if lat_long>0:
-        return  (lat_long['latitude'],lat_long['longitude'])
+    lat_long = LatLongCollection.find_one({'_id': address})
+    if lat_long > 0:
+        return (lat_long['latitude'], lat_long['longitude'])
     try:
-        lat_long = geolocator.geocode(address,language='en')
+        lat_long = geolocator.geocode(address, language='en')
     except:
         return ("", "")
-        #Uncomment to retry 1 more time.
+        # Uncomment to retry 1 more time.
         # try:
         #     # time.sleep(1)
         #     lat_long=geolocator.geocode(address, language='en')
@@ -45,31 +62,36 @@ def get_lat_long(address):
 
     LatLongMap = {'_id': address, 'latitude': lat, 'longitude': long}
     LatLongCollection.insert(LatLongMap)
-    return (lat,long)
+    logger.info('Cached lat long for {}'.format(address))
+
+    return (lat, long)
+
 
 def get_normalized_country_name(countryAddress):
     normalizedCountryNamesCollection = mongoClient['countryDB']['normalizedCountryNames']
 
-    location=normalizedCountryNamesCollection.find_one({'_id': countryAddress})
+    location = normalizedCountryNamesCollection.find_one({'_id': countryAddress})
     if location:
         return location['normal_value']
 
     try:
         location = geolocator.geocode(countryAddress, language='en')
     except:
-        try:
-            # time.sleep(1)
-            location=geolocator.geocode(countryAddress, language='en')
-        except Exception as e:
-            print e
-            return "Not identified"
+        return "Not identified"
+        # Uncomment to retry 1 more time.
+        # try:
+        #     # time.sleep(1)
+        #     location=geolocator.geocode(countryAddress, language='en')
+        # except Exception as e:
+        #     print e
+        #     return "Not identified"
 
     if location != None:
         for addrComponent in location.raw['address_components']:
             if 'country' in addrComponent['types']:
-                longName=addrComponent['long_name']
-                countryCode=addrComponent['short_name']
-                countryMap = {'_id': countryAddress, 'normal_value': countryCode,'long_name':longName}
+                longName = addrComponent['long_name']
+                countryCode = addrComponent['short_name']
+                countryMap = {'_id': countryAddress, 'normal_value': countryCode, 'long_name': longName}
                 normalizedCountryNamesCollection.insert(countryMap)
                 return countryCode
             else:
@@ -77,31 +99,33 @@ def get_normalized_country_name(countryAddress):
                 normalizedCountryNamesCollection.insert(countryMap)
                 return "Not identified"
     else:
-        countryMap = {'_id': countryAddress, 'normal_value': 'Not identified','long_name':'Not identified'}
+        countryMap = {'_id': countryAddress, 'normal_value': 'Not identified', 'long_name': 'Not identified'}
         normalizedCountryNamesCollection.insert(countryMap)
         return "Not identified"
+    logger.info('Cached Country code for {}'.format(countryAddress))
 
 
+def load_content(DB, Country, id, row):
+    Collection = DB[Country]
 
+    if Country != "Not identified":
+        DB["Not identified"].find_one_and_delete({'_id': id})
 
+    oldRow = Collection.find_and_modify(query={'_id': id}, update={'$set': row}, upsert=True, full_response=True)
 
-def load_content(DB,Country,id,row):
-    Collection=DB[Country]
-
-    if Country!="Not identified":
-        DB["Not identified"].find_one_and_delete({'_id':id})
-
-    oldRow=Collection.find_and_modify(query={'_id': id}, update={'$set':row}, upsert=True, full_response=True)
-
-    oldRow = oldRow.pop('value',None)
-    print "yay"
+    oldRow = oldRow.pop('value', None)
     if (oldRow == None):
+        logger.info('New document added to EntityDB.')
         return
     oldRow.pop("_id", None)
-    if sorted(oldRow.items())!=sorted(row.items()):
-        entry={'timestamp':datetime.datetime.now().isoformat(),'collection':Collection.full_name,'old_value':oldRow,'new_value':row}
+    if sorted(oldRow.items()) != sorted(row.items()):
+        logger.info('Entry modified in EntityDB, changes recorded in changesTimelineDB.')
+        entry = {'timestamp': datetime.datetime.now().isoformat(), 'collection': Collection.full_name,
+                 'old_value': oldRow, 'new_value': row}
         changesTimelineDB = mongoClient['changesTimelineDB']
         changesTimelineDB['changesTimeline'].insert(entry)
+    else:
+        logger.info('Document unchanged')
 
 
 def loadOFL(EntityDB, row):
@@ -110,17 +134,18 @@ def loadOFL(EntityDB, row):
         countries = row.get('countries').split(";")
 
     addressess = []
-    all_address=""
+    all_address = ""
     if row.get('address'):
-        all_address=row.get('address')
+        all_address = row.get('address')
         addressess = row.get('address').split(";")
 
-    row['addressess'] = all_address
+    row['addresses'] = all_address
 
     index = 0
     if (len(countries) == 0):
         id = "OFL_" + row['node_id']
-        load_content(EntityDB,"Not identified", id, row)
+        load_content(EntityDB, "Not identified", id, row)
+        return
 
     while (index < len(countries)):
         country = countries[index]
@@ -130,13 +155,12 @@ def loadOFL(EntityDB, row):
                 address = ""
         else:
             address = addressess[index]
-        row['address']=address
-
+        row['address'] = address
 
         if address:
-            lat,long=get_lat_long(address)
-            row['latitude']=lat
-            row['longitude']=long
+            lat, long = get_lat_long(address)
+            row['latitude'] = lat
+            row['longitude'] = long
         else:
             row['latitude'] = ""
             row['longitude'] = ""
@@ -144,9 +168,8 @@ def loadOFL(EntityDB, row):
         index += 1
 
         countryCode = get_normalized_country_name(country)
-        id = u''.join(filter(None,("OFL_",str(row.get('node_id')),row.get('address'),country)))
-        load_content(EntityDB,countryCode,id, row)
-
+        id = u''.join(filter(None, ("OFL_", str(row.get('node_id')), row.get('address'), country)))
+        load_content(EntityDB, countryCode, id, row)
 
 
 def processOffshoreLeaks(offshoreLeaksFilePointer):
@@ -154,29 +177,33 @@ def processOffshoreLeaks(offshoreLeaksFilePointer):
     data = pd.read_csv(offshoreLeaksFilePointer)
     data_json = json.loads(data.to_json(orient='records'))
 
-
     for row in data_json:
-        ex.submit(loadOFL, EntityDB,row)
+        ex.submit(loadOFL, EntityDB, row)
         # loadOFL(EntityDB,row)
+    logger.info('OFFSHORE LEAKS Jobs submitted!!')
+
 
 def loadEXP(EntityDB, row):
-
     addressess = []
     if row.get('addresses'):
         addressess = row.get('addresses').split(";")
 
     index = 0
     if (len(addressess) == 0):
-        Country="Not identified"
+        Country = "Not identified"
         row["address"] = ""
-        id=u''.join(filter(None,("EXP_",row.get("name"),row.get('source_information_url'),str(row.get('entity_number')),row.get('address'),row.get('federal_register_notice'))))
-        load_content(EntityDB,Country,id, row)
+        id = u''.join(filter(None, (
+        "EXP_", row.get("name"), row.get('source_information_url'), str(row.get('entity_number')), row.get('address'),
+        row.get('federal_register_notice'))))
+        load_content(EntityDB, Country, id, row)
+        return
 
     while (index < len(addressess)):
         address = addressess[index]
         row["address"] = address
-        id=u''.join(filter(None,("EXP_",row.get("name"),row.get('source_information_url'),str(row.get('entity_number')),row.get('address'),row.get('federal_register_notice'))))
-
+        id = u''.join(filter(None, (
+        "EXP_", row.get("name"), row.get('source_information_url'), str(row.get('entity_number')), row.get('address'),
+        row.get('federal_register_notice'))))
 
         countryCode = get_normalized_country_name(address)
 
@@ -188,42 +215,48 @@ def loadEXP(EntityDB, row):
             row['latitude'] = ""
             row['longitude'] = ""
 
-
         index += 1
 
-        load_content(EntityDB,countryCode,id,row)
-
+        load_content(EntityDB, countryCode, id, row)
 
 
 def processEXP_GOV_ConsolidatedScrnList(EXP_GOV_ConsolidatedScrnList):
     EntityDB = mongoClient['EntityDB']
-    data=pd.read_csv(BytesIO(EXP_GOV_ConsolidatedScrnList.decode('utf8', 'ignore').encode('utf-8')))
+    data = pd.read_csv(BytesIO(EXP_GOV_ConsolidatedScrnList.decode('utf8', 'ignore').encode('utf-8')))
     data_json = json.loads(data.to_json(orient='records'))
 
     for row in data_json:
         # loadEXP(EntityDB, row)
         ex.submit(loadEXP, EntityDB, row)
+    logger.info('US Consolidated Screening List Jobs submitted!!')
 
 
-if __name__ == "__main__":
-    try:
-        with eventlet.Timeout(10):
-            OFLzipContent=requests.get("https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_offshore_leaks.2017-12-19.zip#_ga=2.164222911.1415007272.1515343932-1495118144.1515130262")
-        fp = StringIO(OFLzipContent.content)
-        zipContent=None
-        zfp=zipfile.ZipFile(fp,'r')
-        OFLfp=zfp.open("offshore_leaks.nodes.entity.csv")
-        processOffshoreLeaks(OFLfp)
-    except Exception as e:
-        print e
-        pass
-    try:
-        with eventlet.Timeout(10):
-            EXP_GOV_ConsolidatedScrnList=requests.get("https://api.trade.gov/consolidated_screening_list/search.csv?api_key=OHZYuksFHSFao8jDXTkfiypO").content
-        processEXP_GOV_ConsolidatedScrnList(EXP_GOV_ConsolidatedScrnList)
-    except Exception as e:
-        print e
-        pass
+try:
+    with eventlet.Timeout(10):
+        logger.info('Processing OFFSHORE LEAKS')
+        IPLINK = "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_offshore_leaks.2017-12-19.zip#_ga=2.164222911.1415007272.1515343932-1495118144.1515130262"
+        logger.info('Downloading {}'.format(IPLINK))
+        OFLzipContent = requests.get(IPLINK)
+    fp = StringIO(OFLzipContent.content)
+    zipContent = None
+    zfp = zipfile.ZipFile(fp, 'r')
+    logger.info('Unzipping contents')
+    OFLfp = zfp.open("offshore_leaks.nodes.entity.csv")
+    processOffshoreLeaks(OFLfp)
+except Exception as e:
+    logger.info('OFFSHORE LEAKS link Timed out')
+    logger.info(e)
 
+try:
+    with eventlet.Timeout(10):
+        logger.info('Processing US Consolidated Screening List')
+        IPLINK = "https://api.trade.gov/consolidated_screening_list/search.csv?api_key=OHZYuksFHSFao8jDXTkfiypO"
+        logger.info('Downloading {}'.format(IPLINK))
+        EXP_GOV_ConsolidatedScrnList = requests.get(IPLINK).content
+    processEXP_GOV_ConsolidatedScrnList(EXP_GOV_ConsolidatedScrnList)
+except Exception as e:
+    logger.info('US Consolidated Screening List link Timed out')
+    logger.info(e)
 
-
+ex.shutdown()
+logger.info('DONE!!')
